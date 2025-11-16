3c619fa1cc67323e26502c662a9ce229159fb72f
hortuadev <hortuadev@gmail.com>
2025-11-16 15:47:40 +0000

    feat(App): agregado el experimento dos

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import net.salesianos.sincronized.Store;
 import net.salesianos.threads.Client;
 import net.salesianos.threads.Manufacturer;
@@ -12,6 +14,45 @@ public class App {
 
         Manufacturer productor = new Manufacturer("uno", 5, store);
         Client consumidor = new Client("uno", 5, store);
+
+        productor.start();
+        consumidor.start();
+
+        productor.join();
+        consumidor.join();
+    }
+
+    public static void experimentTwo() throws Exception {
+        Store store = new Store(5);
+
+        Manufacturer productor = new Manufacturer("rapido", 5, store) {
+            @Override
+            public void run() {
+                Random rand = new Random();
+                for (int i = 0; i < getTotalManufacturingProducts(); i++) {
+                    try {
+                        Thread.sleep(rand.nextInt(200) + 50); // ⚡ 50–250 ms
+                        String finalProduct = "Producto rápido #" + i;
+                        store.saveProduct(finalProduct);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        };
+
+        Client consumidor = new Client("lento", 5, store) {
+            @Override
+            public void run() {
+                Random rand = new Random();
+                for (int i = 0; i < getTotalProductsToConsume(); i++) {
+                    try {
+                        store.deleteProduct();
+                        Thread.sleep(rand.nextInt(1000) + 500);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        };
 
         productor.start();
         consumidor.start();
