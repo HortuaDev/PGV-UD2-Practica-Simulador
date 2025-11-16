5aadf62aa4b167d86fb01d25477e1f7aad692701
hortuadev <hortuadev@gmail.com>
2025-11-16 15:45:33 +0000

    feat(App): agregado el experimento uno

--- src/App.java
+++ src/App.java
@@ -1,7 +1,23 @@
+import net.salesianos.sincronized.Store;
+import net.salesianos.threads.Client;
+import net.salesianos.threads.Manufacturer;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    }
+
+    public static void experimetOne() throws Exception {
+        Store store = new Store(5);
+
+        Manufacturer productor = new Manufacturer("uno", 5, store);
+        Client consumidor = new Client("uno", 5, store);
+
+        productor.start();
+        consumidor.start();
+
+        productor.join();
+        consumidor.join();
     }
 
 }
