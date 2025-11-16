02225ac62d005666027e61bc3d57d6ec7d13d3ef
hortuadev <hortuadev@gmail.com>
2025-11-16 15:03:29 +0000

    feat(Client): creadas las propiedades de la clase

--- src/net/salesianos/threads/Client.java
+++ src/net/salesianos/threads/Client.java
@@ -1,5 +1,11 @@
 package net.salesianos.threads;
 
+import net.salesianos.sincronized.Store;
+
 public class Client extends Thread {
+
+    private final String name;
+    private final int totalProductsToConsume;
+    private final Store store;
 
 }
