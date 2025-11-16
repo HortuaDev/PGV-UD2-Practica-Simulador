4aa98424a80baad3f9f19ffad00e8eb992d89aba
hortuadev <hortuadev@gmail.com>
2025-11-16 15:04:07 +0000

    feat(Client): creado el constructor de la clase

--- src/net/salesianos/threads/Client.java
+++ src/net/salesianos/threads/Client.java
@@ -8,4 +8,10 @@ public class Client extends Thread {
     private final int totalProductsToConsume;
     private final Store store;
 
+    public Client(String name, int totalProductsToConsume, Store store) {
+        this.name = name;
+        this.totalProductsToConsume = totalProductsToConsume;
+        this.store = store;
+        setName("Cliente-" + name);
+    }
 }
