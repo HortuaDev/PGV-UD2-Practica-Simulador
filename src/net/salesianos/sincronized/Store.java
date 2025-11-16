bb82da45742bc8b9840fa07ce7ea46b14aa8287b
hortuadev <hortuadev@gmail.com>
2025-11-16 14:59:22 +0000

    feat(Sore): creado el contructor

--- src/net/salesianos/sincronized/Store.java
+++ src/net/salesianos/sincronized/Store.java
@@ -1,9 +1,15 @@
 package net.salesianos.sincronized;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Store extends Thread {
 
     private final int maxSize;
     private final List<String> products;
+
+    public Store(int maxSize) {
+        this.maxSize = maxSize;
+        this.products = new ArrayList<>();
+    }
 }
