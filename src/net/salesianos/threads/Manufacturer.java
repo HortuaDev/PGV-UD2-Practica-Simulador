78e6013d5e587c83316b857dc0ef370c485a4f8a
hortuadev <hortuadev@gmail.com>
2025-11-16 14:54:03 +0000

    feat(Manufacturer): agregadas las propiedades de la clase

--- src/net/salesianos/threads/Manufacturer.java
+++ src/net/salesianos/threads/Manufacturer.java
@@ -2,4 +2,13 @@ package net.salesianos.threads;
 
 public class Manufacturer {
 
+    private final int totalManufacturingProducts;
+    private final String name;
+    private final Store store;
+
+    private final String[] typeProducts = {
+            "Lacteos", "Carnes", "Frutas", "Verduras", "Bebidas",
+            "Snacks", "Panaderia", "Limpieza", "Higiene", "Congelados"
+    };
+
 }
