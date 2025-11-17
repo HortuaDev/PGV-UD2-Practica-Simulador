c2b15cdf1abb7d9d0ce8369de0d9115ef5f2d9da
hortuadev <hortuadev@gmail.com>
2025-11-17 09:21:27 +0000

    fix(App): corregida la estructura del esperimento uno

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 
 import net.salesianos.sincronized.Store;
 import net.salesianos.threads.Client;
@@ -8,8 +7,6 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         experimentoUno();
-        // experimentTwo();
-        // experimentThree();
 
     }
 
@@ -19,61 +16,6 @@ public class App {
         Manufacturer productor = new Manufacturer("P1", 5, store);
 
         Client consumidor = new Client("C1", 5, store);
-
-        productor.start();
-        consumidor.start();
-
-        productor.join();
-        consumidor.join();
-    }
-
-    public static void experimentTwo() throws Exception {
-        Store store = new Store(5);
-
-        Manufacturer productor = new Manufacturer("rapido", 5, store) {
-            @Override
-            public void run() {
-                Random rand = new Random();
-                for (int i = 0; i < getTotalManufacturingProducts(); i++) {
-                    try {
-                        Thread.sleep(rand.nextInt(200) + 50); // ⚡ 50–250 ms
-                        String finalProduct = "Producto rápido #" + i;
-                        store.saveProduct(finalProduct);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-        };
-
-        Client consumidor = new Client("lento", 5, store) {
-            @Override
-            public void run() {
-                Random rand = new Random();
-                for (int i = 0; i < getTotalProductsToConsume(); i++) {
-                    try {
-                        store.deleteProduct();
-                        Thread.sleep(rand.nextInt(1000) + 500);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-        };
-
-        productor.start();
-        consumidor.start();
-
-        productor.join();
-        consumidor.join();
-    }
-
-    public static void experimentThree() throws Exception {
-        Store store = new Store(5);
-
-        Manufacturer productor = new Manufacturer("alta-prioridad", 5, store);
-        Client consumidor = new Client("normal", 5, store);
-
-        productor.setPriority(Thread.MAX_PRIORITY);
-        consumidor.setPriority(Thread.NORM_PRIORITY);
 
         productor.start();
         consumidor.start();
