a6ae0a57583233149dff3627652d2d09d9c4f81f
hortuadev <hortuadev@gmail.com>
2025-11-17 09:24:12 +0000

    fix(App): corregida la estructura del metodo experimento tres

--- src/App.java
+++ src/App.java
@@ -7,62 +7,8 @@ import net.salesianos.threads.Manufacturer;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // experimetOne();
-        // experimentTwo();
         experimentoTres();
 
-    }
-
-    public static void experimetOne() throws Exception {
-        Store store = new Store(5);
-
-        Manufacturer productor = new Manufacturer("uno", 5, store);
-        Client consumidor = new Client("uno", 5, store);
-
-        productor.start();
-        consumidor.start();
-
-        productor.join();
-        consumidor.join();
-    }
-
-    public static void experimentTwo() throws Exception {
-        Store store = new Store(5);
-
-        Manufacturer productor = new Manufacturer("rapido", 5, store) {
-            @Override
-            public void run() {
-                Random rand = new Random();
-                for (int i = 0; i < getTotalManufacturingProducts(); i++) {
-                    try {
-                        Thread.sleep(rand.nextInt(200) + 50); // ⚡ 50–250 ms
-                        String finalProduct = "Producto rápido #" + i;
-                        store.saveProduct(finalProduct);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-        };
-
-        Client consumidor = new Client("lento", 5, store) {
-            @Override
-            public void run() {
-                Random rand = new Random();
-                for (int i = 0; i < getTotalProductsToConsume(); i++) {
-                    try {
-                        store.deleteProduct();
-                        Thread.sleep(rand.nextInt(1000) + 500);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-        };
-
-        productor.start();
-        consumidor.start();
-
-        productor.join();
-        consumidor.join();
     }
 
     public static void experimentoTres() throws Exception {
